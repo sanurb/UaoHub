bdd6f561abcf02cf17ef8fc307ea9566612a3db2
Santiago Urbano <david.urbano@uao.edu.co>
2023-04-08 17:16:42 -0500

    styles: add animation on sidebar and dropdown

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,10 @@ import PostCard from '@/components/PostCard';
 export default function Home() {
 	return (
 		<div className="flex mt-4 max-w-4xl mx-auto gap-6">
-			<div className="w-1/3">
+			<div className="w-3/12">
 				<NavigationCard />
 			</div>
-			<div className="grow">
+			<div className="w-10/12">
 				<PostFormCard />
 				<PostCard />
 			</div>
