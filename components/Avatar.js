6a60f7d7a3fe06d85d006d814c7eefeaf7dcf9c9
Santiago Urbano <david.urbano@uao.edu.co>
2023-04-09 00:17:07 -0500

    feat(Avatar.js): add support for different sizes of avatar feat(saved.js): create SavedPostsPage component with Layout and PostCard components

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,6 +1,10 @@
-export default function Avatar() {
+export default function Avatar({ size }) {
+	let width = 'w-12';
+	if (size === 'lg') {
+		width = 'w-24 md:w-36';
+	}
 	return (
-		<div className="w-12 rounded-full overflow-hidden">
+		<div className={`${width} rounded-full overflow-hidden`}>
 			<img
 				src="https://images.unsplash.com/photo-1623605931891-d5b95ee98459?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=894&q=80"
 				alt="avatar"
