61f3ec5e82e4db568f91915cc7c825caf8b4c68e
Santiago Urbano <david.urbano@uao.edu.co>
2023-04-09 07:43:59 -0500

    refactor(_document.js): remove unused file refactor(index.js): use Layout component instead of NavigationCard and divs
    feat(notifications.js): add NotificationsPage component with notification cards and links to user profiles and photos
    
    feat(Home.module.css): add styles for the home page container, main section, footer, title, description, code, grid, and card elements. Add media queries for responsive design and dark mode support.

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,12 @@
-import NavigationCard from '@components/NavigationCard';
+import Layout from '@components/Layout';
 import PostFormCard from '@/components/PostFormCard';
 import PostCard from '@/components/PostCard';
 
 export default function Home() {
 	return (
-		<div className="flex mt-4 max-w-4xl mx-auto gap-6">
-			<div className="w-3/12">
-				<NavigationCard />
-			</div>
-			<div className="w-10/12">
-				<PostFormCard />
-				<PostCard />
-			</div>
-		</div>
+		<Layout>
+			<PostFormCard />
+			<PostCard />
+		</Layout>
 	);
 }
