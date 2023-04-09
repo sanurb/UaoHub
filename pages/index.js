690746d4e802b429efc3d5efda3955a3ba0f911d
Santiago Urbano <david.urbano@uao.edu.co>
2023-04-09 15:36:33 -0500

    feat: add Supabase authentication to the app feat: add Supabase client and session context provider to _app.js feat: add login page to redirect unauthenticated users fix: add Supabase dependencies to package.json

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
 import Layout from '@components/Layout';
 import PostFormCard from '@/components/PostFormCard';
 import PostCard from '@/components/PostCard';
+import { useSession } from '@supabase/auth-helpers-react';
+import LoginPage from './login';
 
 export default function Home() {
+	const session = useSession();
+
+	if(!session){
+		return <LoginPage />
+	}
+
 	return (
 		<Layout>
 			<PostFormCard />
